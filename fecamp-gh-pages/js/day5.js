1dbc73b8968d9bb1b5fb8578d650b3ebd0c35d37
watereel <watereel@gmail.com>
2015-03-26 21:26:16 +0900

    2015-03-25-Backup

---
+++ fecamp-gh-pages/js/day5.js
@@ -0,0 +1,15 @@
+var num
+num -= 6;
+if (num > 3){
+	console.log('num은 3보다 크다.');
+} else {
+	console.log('num은 3보다 크지않다.');
+}
+
+if (num > 3){
+	console.log('num은 3보다 크다.');
+} else if (num > 0){
+	console.log('num은 3보다 크진 않아도 0보다는 크다.');
+} else {
+	console.log('num은 3보다 크지않다.')
+}
